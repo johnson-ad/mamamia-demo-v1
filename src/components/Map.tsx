62247deaf0d02c5de8c372ab2c9ed493a2196325
johnson-ad <one007stone@gmail.com>
2025-06-29 19:26:41 +0200

    update v5

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,38 +1,47 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import Map, { Marker, Popup } from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { useState } from 'react';
 
-// Correction de l'erreur d'icône avec typage TypeScript
-const DefaultIcon = L.icon({
-  iconUrl: '/leaflet/marker-icon.png',
-  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
-  shadowUrl: '/leaflet/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
+export default function MapLibreMap() {
+  const [showPopup, setShowPopup] = useState(true);
 
-L.Marker.prototype.options.icon = DefaultIcon;
-
-export default function Map() {
   return (
-    <MapContainer 
-      center={[48.8566, 2.3522]} 
-      zoom={13} 
-      style={{ height: '400px', width: '100%' }}
-    >
-      {/* Correction des props TileLayer */}
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      
-      <Marker position={[48.8566, 2.3522]}>
-        <Popup>Paris, France</Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ height: '400px', width: '100%' }}>
+      <Map
+        initialViewState={{
+          longitude: 2.3522,
+          latitude: 48.8566,
+          zoom: 13,
+        }}
+        mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+        style={{ width: '100%', height: '100%' }}
+      >
+        <Marker
+          longitude={2.3522}
+          latitude={48.8566}
+          anchor="bottom"
+        >
+          <img
+            src="/map-marker.svg"
+            alt="marker"
+            style={{ width: 25, height: 25, cursor: 'pointer' }}
+            onClick={() => setShowPopup(true)}
+          />
+        </Marker>
+
+        {showPopup && (
+          <Popup
+            longitude={2.3522}
+            latitude={48.8566}
+            onClose={() => setShowPopup(false)}
+            closeOnClick={false}
+          >
+            <div>Paris, France</div>
+          </Popup>
+        )}
+      </Map>
+    </div>
   );
 }
